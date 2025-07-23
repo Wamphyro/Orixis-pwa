721dad15d0fb3a5fde5ed2bc494e93e7da2afd15
Wamphyro <106920971+Wamphyro@users.noreply.github.com>
2025-07-23 14:58:22 +0200

    Create auth-config.js

---
+++ auth-config.js
@@ -0,0 +1,15 @@
+// auth-config.js - Configuration des magasins et mots de passe
+const MAGASINS = {
+    "9DIJ": {
+        nom: "Paris - Dijon",
+        passwordHash: "a665a45920422f9d417e4867efdc4fb8a04a1f3fff1fa07e998e86f7f7a27ae3" // 123
+    },
+    "9AVA": {
+        nom: "Paris - Avignon", 
+        passwordHash: "03ac674216f3e15c761ee1a5e255f067953623c8b388b4459e13f978d7c846f4" // 1234
+    },
+    "9LYN": {
+        nom: "Paris - Lyon",
+        passwordHash: "5994471abb01112afcc18159f6cc74b4f511b99806da59b3caf5a9c173cacfc5" // 12345
+    }
+};
