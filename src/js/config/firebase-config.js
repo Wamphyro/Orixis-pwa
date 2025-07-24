9e9ce94d2bbb50c0d8292b53c06da5b3ffe31813
Wamphyro <106920971+Wamphyro@users.noreply.github.com>
2025-07-24 13:37:49 +0200

    Create firebase-config.js

---
+++ src/js/config/firebase-config.js
@@ -0,0 +1,13 @@
+// Configuration Firebase pour SAV Audio
+const firebaseConfig = {
+  apiKey: "AIzaSyBTHGWZt-Eg2gG8nLNs8aWgEw6evBIOeZ4",
+  authDomain: "orixis-pwa.firebaseapp.com",
+  projectId: "orixis-pwa",
+  storageBucket: "orixis-pwa.firebasestorage.app",
+  messagingSenderId: "410837125332",
+  appId: "1:410837125332:web:b2eeebb976fb10eeb59341",
+  measurementId: "G-PJ8K3Q4W4E"
+};
+
+// Export pour utilisation dans d'autres fichiers
+export { firebaseConfig };
