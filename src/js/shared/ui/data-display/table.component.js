de4e6ff4b6d7193b345309efa280db4187d40da0
Wamphyro <106920971+Wamphyro@users.noreply.github.com>
2025-07-28 19:15:06 +0200

    Ajout du module de gestion des commandes - HTML/CSS

--- src/js/shared/ui/data-display/table.component.js
+++ src/js/shared/ui/data-display/table.component.js
@@ -1647,6 +1647,18 @@ const TableComponent = (() => {
     let instanceCounter = 0;
 
     /**
+ * Convertir une chaîne en couleur
+ */
+function stringToColor(str) {
+    let hash = 0;
+    for (let i = 0; i < str.length; i++) {
+        hash = str.charCodeAt(i) + ((hash << 5) - hash);
+    }
+    const hue = hash % 360;
+    return `hsl(${hue}, 70%, 50%)`;
+}
+
+    /**
      * Classe Table
      */
     class UITable {
